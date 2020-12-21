dc500f7fc4554b1503d7c7bb24ca22182fb99155
Paulo Aragao <aragao.paulo@gmail.com>
2020-12-21 19:51:17 -0300

    functions working

--- flowsensor.py
+++ flowsensor.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 import time,sys
 
-FLOW_SENSOR = 7
+FLOW_SENSOR = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -12,20 +12,19 @@ global count
 count = 0
 
 def countPulse(channel):
+    global count
     count += 1
-    print('revs: {}'.format(count))
 
-GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=countPulse)
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
 
 while True:
     try:
         start_counter = 1
         time.sleep(1)
         start_counter = 0
-        flow = (count * 60 * 2.25 / 1000)
+        flow = round((count / 7.5), 2)
         print('The flow is: {} Liter/min'.format(flow))
-        count = 0
-        time.sleep(3)
+        count = 0 # every second we output the flow and zero the counter
     except KeyboardInterrupt:
         print('\ncaught keyboard interrupt!, bye')
         GPIO.cleanup()
